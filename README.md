36322ff2ca6c573b5be5410b64c2861a1c5f2b17
salagata <122649005+salagata@users.noreply.github.com>
2026-03-07 20:52:13 -0500

    Added an CLI for NPX, still finding out how to publish it lol

--- README.md
+++ README.md
@@ -1,5 +1,95 @@
 # NotSoBot-TagScript-Include
 
 Add the {include} command in TagScript to handle big projects, and have the project through multiple files.
-{include} works as #include in C, replacing the content of a file, this compiler will scan the code for {include}
-commands, and clone the code into a big file
+{include} works as #include in C, but with single files instead of header files, replacing the content of a file, this compiler will scan the code for {include}
+commands, and clone the code into a big file.
+
+## Syntaxes
+
+### {include:FILE}
+FILE: A file.
+
+## Install the CLI
+
+You may install it by NPM
+```
+npm install notsobot-tagscript-include
+```
+
+## Use the CLI
+```
+npx nsb-i <file> [option]
+```
+
+To keep it simple, you can only use one option at a time.
+Not adding an option will show a preview of the transpiled code But it will not write it in any file.
+
+    -o <outputFile>
+        Writes the transpiled code in the file specified in <outputFile>
+    -O
+        Overwrite the original file, use this option with caution, is recommended to use -o <outputFile> to prevent this.
+    -p <directory>
+        Writes the transpiled code with the same name as the main file, but in a different directory, if there isn't, a new directory will be created.
+    -P
+        Writes the transpiled code with the same name but inside an out/ directory, if there isn't, a new directory will be created.
+    --help
+        Shows the help list.
+### Writing in a file
+
+```
+npx nsb-i <file> -o <outputFile>
+```
+<file>: The file to transpile
+<outputFile>: The output file
+If the output file is inside a directory, make sure the directory exists.
+
+### Overwritting a file
+
+```
+npx nsb-i <file> -O
+```
+<file>: The file to transpile
+**Warning**: This overwrites the current file, use -o <outputFile> if you don't want to overwrite the file.
+If the output file is inside a directory, make sure the directory exists.
+
+### Writing in a directory
+
+```
+npx nsb-i <file> -p <directory>
+```
+<file>: The file to transpile
+<directory>: The target directory
+This will keep the name of the original file, very recommended.
+If the directory doesn't exist, a new directory will be created.
+
+```
+npx nsb-i <file> -P
+```
+<file>: The file to transpile
+Does the same as -p but in an `out` directory.
+If the directory doesn't exist, a new directory will be created.
+
+### Preview
+
+```
+npx nsb-i <file>
+```
+<file>: The file to transpile
+Not adding any option will show a preview of the file transpiled, if you don't want to write a new file,
+
+## Examples
+
+There is an example of a program with dependences in the directory example/
+
+```
+npx nsb-i main.nsb
+```
+```
+npx nsb-i main.nsb -o out.nsb
+```
+```
+npx nsb-i lib.nsb -p dist
+```
+```
+npx nsb-i lib.nsb -P
+```
--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -1,8 +1,72 @@
 #!/usr/bin/env node
 
-import { parse } from "../src/parse";
+import { parse } from "../parse";
+import { writeFile, mkdir } from "fs/promises";
+import { resolve, basename } from "path";
 import process from "process";
 
-const args = process.argv.slice(2);
+async function main() {
+    const args = process.argv.slice(2);
 
-parse(args);
+    if (args.length < 1) {
+        console.log("Usage: npx nsb-i <file> [option]");
+        console.log("Not adding an option will show a preview of the transpiled code But it will not write it in any file.");
+        console.log("use -O to overwrite the current file (Hard)");
+        console.log("use -o <outputFile> to write into a different file (Soft)");
+        console.log("use --help to see the avaiable options");
+        return;
+    }
+
+    if (args[0] === "--help") {
+        console.log("Avaiable options:");
+        console.log("You can apply only one option at a time");
+        console.log("\t-o <outputFile>");
+        console.log("\t\tWrites the transpiled code in the file specified in <outputFile>");
+        console.log("\t-O");
+        console.log("\t\tOverwrite the original file, use this option with caution, is recommended to use -o <outputFile> to prevent this.");
+        console.log("\t-p <directory>");
+        console.log("\t\tWrites the transpiled code with the same name as the main file, but in a different directory, if there isn't, a new directory will be created.");
+        console.log("\t-P");
+        console.log("\t\tWrites the transpiled code with the same name but inside an out/ directory, if there isn't, a new directory will be created.");
+        console.log("\t--help");
+        console.log("\t\tShows this list.");
+        return;
+    }
+
+    const thisFile = args[0];
+    const thisFileName = basename(thisFile);
+
+    try {
+        const { text } = await parse(`{include:${thisFile}}`);
+
+        const opt = args[1];
+        if (opt === "-o") {
+            if (args.length < 3) throw new Error("One file must be specified.");
+            const outputFile = args[2];
+            await writeFile(outputFile, text);
+        } else if (opt === "-O") {
+            await writeFile(thisFile, text);
+        } else if (opt === "-p") {
+            if (args.length < 3) throw new Error("One directory must be specified.");
+            const directory = args[2];
+            await mkdir(directory, { recursive: true });          // ← crea el directorio
+            const outputFile = resolve(directory, thisFileName);
+            await writeFile(outputFile, text);
+        } else if (opt === "-P") {
+            const directory = "out";
+            await mkdir(directory, { recursive: true });          // ← idem
+            const outputFile = resolve(directory, thisFileName);
+            await writeFile(outputFile, text);
+        } else {
+            if (opt) {
+                console.warn(`${opt} is not a valid option, use --help to see the list of valid options`);
+            }
+            console.log(text);
+        }
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+}
+
+main();
